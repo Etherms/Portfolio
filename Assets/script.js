270d6d88faaa5315441cefa4ad36b4e83e1b2646
Etherms <hermosaedson459@gmail.com>
2023-07-14 22:40:55 +0800

    Added font-family

---
+++ Assets/script.js
@@ -0,0 +1,24 @@
+
+var Dovalue = document.querySelector(".do-value");
+
+function function1() {
+    Dovalue.textContent = "Websites!";
+}
+function function2() {
+    Dovalue.textContent = "Games!";
+}
+function function3() {
+    Dovalue.textContent = "Software!";
+}
+
+var functions = [function1, function2, function3];
+var index = 0;
+
+var intervalId = setInterval(function() {
+  functions[index](); // Call the function at the current index
+
+  index++; // Move to the next index
+  if (index >= functions.length) {
+    index = 0; // Reset the index if it exceeds the array length
+  }
+}, 3000);
